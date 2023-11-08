7ebff1f0afec1d5452dd51cdc71e0839e0f3efcb
simonelopess <sisilopes20@gmail.com>
2023-11-07 21:00:56 -0300

    add functionality to delete task

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,15 @@
     PATCH -> /tasks/:id/complete */
 import { randomUUID } from 'node:crypto';
 import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
+
 
 const database = new Database();
 
 export const routes = [
     {
         method: 'GET',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const tasks = database.select('tasks');
             return res.end(JSON.stringify(tasks))
@@ -20,7 +22,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const { title, description } = req.body || {};
 
@@ -39,6 +41,15 @@ export const routes = [
             }
             return res.writeHead(404).end('Missing information! Title or description not found')
 
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+            database.delete('tasks', id);
+            return res.writeHead(204).end()
         }
     }
 ]
